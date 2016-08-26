358d1793f687e6a4c0df6bf62c96b279ee2b10b8
Lauren Smith <laurensmith@comcast.net>
2016-08-26 09:34:40 -0500

    Get words

--- word-search.js
+++ word-search.js
@@ -1,1 +1,27 @@
+const { createReadStream } = require('fs');
+const limitLength = require('./limit-ten');
+
 console.log("hey");
+
+createReadStream('/usr/share/dict/words')
+  // .pipe(limitLength)
+  .pipe(process.stdout);
+
+// const 
+
+// this is the main file
+
+// accept one argument: searched word
+
+// use this file to search for words: 
+// // /usr/share/dict/words
+
+// 1- createReadStream
+
+// 2- split()
+
+// 3- map()
+
+// limit-ten will contain Transform
+// return only 10 words
+// then pipe to process.stdout
