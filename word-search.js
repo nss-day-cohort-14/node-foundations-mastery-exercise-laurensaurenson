4c10ffd8045e24886d71bafd9dd68e3216d75f6e
Lauren Smith <laurensmith@comcast.net>
2016-08-26 11:11:50 -0500

    Usage comment and cleaned code

--- word-search.js
+++ word-search.js
@@ -4,33 +4,19 @@ const es = require('event-stream');
 
 const [,, searchedWord, ..._] = process.argv;
 
-createReadStream('/usr/share/dict/words')
-  .pipe(es.split())
-  .pipe(es.map(function (data, cb) { 
-    if (data.startsWith(`${searchedWord}`)) {
-      cb(null, `${data} `)
-    } else {
-      cb()
-    }
-  }))
-  .pipe(limitLength)
-  .pipe(process.stdout);
-
-// const 
-
-// this is the main file
-
-// accept one argument: searched word
-
-// use this file to search for words: 
-// // /usr/share/dict/words
-
-// 1- createReadStream
-
-// 2- split()
-
-// 3- map()
-
-// limit-ten will contain Transform
-// return only 10 words
-// then pipe to process.stdout
+if (!searchedWord) {
+  console.log(`Usage: word-search.js [searchterm]`);
+} else {
+  console.log(`Usage: word-search.js ${searchedWord}`);
+  createReadStream('/usr/share/dict/words')
+    .pipe(es.split())
+    .pipe(es.map(function (data, cb) { 
+      if (data.startsWith(`${searchedWord}`)) {
+        cb(null, `${data} `)
+      } else {
+        cb()
+      }
+    }))
+    .pipe(limitLength)
+    .pipe(process.stdout);
+}
